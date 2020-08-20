9784b3511e24fd4085858622ba85a3ac461d8f67
tghjkl777 <tghjkl777@gmail.com>
2020-08-20 12:05:40 +0900

    08.20 commit

---
+++ 수학1/no_2839.py
@@ -0,0 +1,16 @@
+n=int(input())
+cnt=0
+
+while(True):
+    if n%5 ==0 :
+        cnt=n/5
+        break
+    else :
+        if n%3 ==0 :
+            cnt=n/3
+            break
+        else :
+
+
+
+
